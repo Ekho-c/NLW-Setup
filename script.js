82ea89af5b140bbd692f0b4f11968bdddc0f2446
ekho-c <gabrielsouza432@gmail.com>
2023-01-22 17:40:55 -0300

    automatização

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  run: ["01-16", "01-17", "01-18", "01-19", "01-20", "01-21", "01-22"],
-  food: ["01-16", "01-17", "01-19", "01-20", "01-22"],
-  workout: ["01-22"],
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso ❌")
+    return
+  }
+
+  alert("Adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
 }
 
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
